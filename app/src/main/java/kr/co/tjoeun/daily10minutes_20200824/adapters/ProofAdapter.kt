c9b93319b251fd456f3ed17a05e24c53f2d5114d
조경진 <kj_cho@idealidea.co.kr>
2020-09-16 21:35:02 +0900

    인증글 이미지 처리

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/adapters/ProofAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200824/adapters/ProofAdapter.kt
@@ -37,6 +37,21 @@ class ProofAdapter(
         val data = mList[position]
 
         proofContentTxt.text = data.content
+        
+//        첨부된 이미지가 있다면 => 이미지뷰를 화면에 표시
+//        인증글에 사진이 아예 없다면 => 이미지뷰 자체를 숨김처리
+
+        if (data.imgList.size == 0) {
+//            사진이 없는 글
+
+            proofFirstImg.visibility = View.GONE
+
+        }
+        else {
+//            하나 이상의 사진이 있는 경우
+            proofFirstImg.visibility = View.VISIBLE
+            Glide.with(mContext).load(data.imgList[0]).into(proofFirstImg)
+        }
 
         return row
     }
